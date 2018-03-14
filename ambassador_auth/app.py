c60ddffdc475c86612d0d238ef76486a5fccec82
plombardi <plombardi89@gmail.com>
2018-03-14 04:36:18 -0400

    basic functionality implemented

--- ambassador_auth/app.py
+++ ambassador_auth/app.py
@@ -1,8 +1,53 @@
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify, Response
+from functools import wraps
+from werkzeug.routing import Rule
 
 app = Flask(__name__)
+
+
+# Specify the extauth route here because Flask requires manual specification of all the HTTP methods on the @app.route
+# decorator which is tedious and prone to break in practice from new or custom HTTP methods being introduced.
+app.url_map.add(Rule("/extauth", strict_slashes=False, endpoint="handle_authorization", defaults={"path": ""}))
+app.url_map.add(Rule("/extauth/<path:path>", endpoint="handle_authorization"))
+
+
+def check_auth(username, password):
+    return username == "admin" and password == "admin"
+
+
+def unauthorized():
+    return Response(status=401)
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        # Favicon is the little icon associated with a domain in a web browser. Browsers issue a request for the
+        # resource /favicon.ico alongside any other HTTP request which pollutes the logs with lots of 404 Not Found logs
+        # because usually the favicon cannot be resolved. This tells the browser to go away; there is no favicon here.
+        if request.path == "/favicon.ico":
+            return Response(status=403)
+
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return unauthorized()
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return Response(status=404)
 
 
 @app.route("/healthz", methods=["GET"])
 def healthz():
     return "OK", 200
+
+
+@app.endpoint("handle_authorization")
+@requires_auth
+def handle_authorization(path):
+    return Response(status=200)
